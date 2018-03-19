bde253f65c39e96e04cd94edeb07391b84c37d5e
Mainul Hasan <mainulhasan@live.com>
2018-03-19 15:46:52 +0530

    errors

---
+++ week-6/task-1/CA.c
@@ -0,0 +1,146 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+#include <string.h>
+#include <assert.h>
+#include <time.h>
+
+#include <openssl/evp.h>
+#include <openssl/err.h>
+#include <openssl/rsa.h>
+#include <openssl/sha.h>
+#include <openssl/rand.h>
+#include <openssl/pem.h> 
+
+const char hn[] = "SHA256";
+
+
+struct CSRToBeSigned{
+	unsigned int deviceID;
+	unsigned int orgID;
+	unsigned char curveID;
+	unsigned char *hashID;
+	unsigned int pubKLen;
+	EC_KEY pubK;
+};
+
+struct MiniCertSR{
+	unsigned int deviceID;
+	unsigned int orgID;
+	unsigned char curveID;
+	unsigned char *hashID;
+	unsigned int pubKLen;
+	char pubK;
+	size_t sLen = 0;
+	char *sig = NULL;
+};
+
+struct CA{
+	unsigned int deviceID;
+	unsigned int orgID;
+	unsigned char curveID;
+	unsigned char hashID;
+	unsigned int pubKLen;
+	EC_KEY pubK;
+	unsigned int certSNo;
+	unsigned int CAID;
+	unsigned __int64 validF;
+	unsigned int validFor;
+	
+};
+
+nt sign_it(byte** sig, size_t* slen, EVP_PKEY* pkey)
+{
+    /* Returned to caller */
+    int result = -1;
+    
+    if(!sig || !pkey) {
+        assert(0);
+        return -1;
+    }
+    
+    if(*sig)
+        OPENSSL_free(*sig);
+    
+    *sig = NULL;
+    *slen = 0;
+    
+    EVP_MD_CTX* ctx = NULL;
+    
+    do
+    {
+        ctx = EVP_MD_CTX_create();
+        assert(ctx != NULL);
+        if(ctx == NULL) {
+            printf("EVP_MD_CTX_create failed, error 0x%lx\n", ERR_get_error());
+            break; /* failed */
+        }
+        
+        const EVP_MD* md = EVP_get_digestbyname(hn);
+        assert(md != NULL);
+        if(md == NULL) {
+            printf("EVP_get_digestbyname failed, error 0x%lx\n", ERR_get_error());
+            break; /* failed */
+        }
+        
+        int rc = EVP_DigestInit_ex(ctx, md, NULL);
+        assert(rc == 1);
+        if(rc != 1) {
+            printf("EVP_DigestInit_ex failed, error 0x%lx\n", ERR_get_error());
+            break; /* failed */
+        }
+        
+        rc = EVP_DigestSignInit(ctx, NULL, md, NULL, pkey);
+        assert(rc == 1);
+        if(rc != 1) {
+            printf("EVP_DigestSignInit failed, error 0x%lx\n", ERR_get_error());
+            break; /* failed */
+        }
+        
+        size_t req = 0;
+        rc = EVP_DigestSignFinal(ctx, NULL, &req);
+        assert(rc == 1);
+        if(rc != 1) {
+            printf("EVP_DigestSignFinal failed (1), error 0x%lx\n", ERR_get_error());
+            break; /* failed */
+        }
+        
+        assert(req > 0);
+        if(!(req > 0)) {
+            printf("EVP_DigestSignFinal failed (2), error 0x%lx\n", ERR_get_error());
+            break; /* failed */
+        }
+        
+        *sig = OPENSSL_malloc(req);
+        assert(*sig != NULL);
+        if(*sig == NULL) {
+            printf("OPENSSL_malloc failed, error 0x%lx\n", ERR_get_error());
+            break; /* failed */
+        }
+        
+        *slen = req;
+        rc = EVP_DigestSignFinal(ctx, *sig, slen);
+        assert(rc == 1);
+        if(rc != 1) {
+            printf("EVP_DigestSignFinal failed (3), return code %d, error 0x%lx\n", rc, ERR_get_error());
+            break; /* failed */
+        }
+        
+        assert(req == *slen);
+        if(rc != 1) {
+            printf("EVP_DigestSignFinal failed, mismatched signature sizes %ld, %ld", req, *slen);
+            break; /* failed */
+        }
+        
+        result = 0;
+        
+    } while(0);
+    
+    if(ctx) {
+        EVP_MD_CTX_destroy(ctx);
+        ctx = NULL;
+    }
+    
+    return !!result;
+}
